680a14ab2c39442130e36884912a32c4616f5764
Shahnazrustamli <shahnazrtm@gmail.com>
2024-05-08 16:44:50 +0400

    VUSALE

--- Shovgu mllm/20-EcommerceWithApi/index.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-let fetchApi =fetch('https://fakestoreapi.com/products') // api for the get request
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-data.sort((a,b) => {a.firstname.localeCompare(b.firstname)})
-         
